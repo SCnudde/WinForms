6241f666ec381579b19fbcf6f2129255bdeed897
SCnudde <125920286+SCnudde@users.noreply.github.com>
2025-12-29 23:52:43 +0500

    fontsize in settings done!

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -78,6 +78,7 @@ namespace Clock
             writer.WriteLine(labelTime.ForeColor.ToArgb());
 
             writer.WriteLine(fontDialog.Filename);
+            writer.WriteLine(labelTime.Font.Size);
 
             writer.Close();
             System.Diagnostics.Process.Start("notepad", "Settings.ini");
@@ -107,7 +108,7 @@ namespace Clock
                 labelTime.BackColor = backgroundColorDialog.Color = Color.FromArgb(Convert.ToInt32(reader.ReadLine()));
                 labelTime.ForeColor = foregroundColorDialog.Color = Color.FromArgb(Convert.ToInt32(reader.ReadLine()));
 
-                fontDialog = new FontDialog(reader.ReadLine());
+                fontDialog = new FontDialog(reader.ReadLine(),reader.ReadLine());
                 labelTime.Font = fontDialog.Font;
 
                 reader.Close();
